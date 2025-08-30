79b35760831339390bd97f3826fab881f6056acd
natkhatishigh <iamshivanshsood@gmail.com>
2025-08-30 14:08:11 +0530

    feat:  initial Spring Boot project + Docker Compose Setup

---
+++ src/main/java/com/example/UndoSchool/UndoSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.UndoSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UndoSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UndoSchoolApplication.class, args);
+	}
+
+}
